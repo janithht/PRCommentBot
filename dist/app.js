018de25e023a4613ad39d0aa29eab87b57079385
janithht <hathnagodahwemjn.20@uom.lk>
2024-02-01 11:01:33 +0530

    update

--- dist/app.js
+++ dist/app.js
@@ -9,7 +9,7 @@ const apiRoutes_1 = __importDefault(require("./routes/apiRoutes"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 app.use(express_1.default.json());
-app.use('/auth', authRoutes_1.default);
+app.use(authRoutes_1.default);
 app.use(apiRoutes_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/auth', authRouter);
+app.use(authRouter);
 app.use(apiRouter);
 
 app.listen(PORT, () => {
